1b2cb98b7b0d35a626f7b84889386ac6ee5256c9
Stephan Chandler-Garcia <stephan@gdprsuperheroes.com>
2020-05-13 07:11:47 -0700

    removal of deprecited Aura Comopnents

--- force-app/main/default/aura/consentCaptureAcquisitionTypeAura/consentCaptureAcquisitionTypeAuraController.js
+++
@@ -1,9 +0,0 @@
-({
-  handleCaptureTypeSet: function(component, event, helper) {
-    console.log(event);
-
-    var captureType = event.Co.value;
-
-    component.set("v.captureType", captureType.length > 0 ? captureType : null);
-  }
-});
--- force-app/main/default/aura/consentCaptureStatusSelectorAura/consentCaptureStatusSelectorAuraController.js
+++
@@ -1,9 +0,0 @@
-({
-  handleStatusTypeSet: function(component, event, helper) {
-    console.log(event);
-
-    var statusType = event.Co.value;
-
-    component.set("v.statusType", statusType.length > 0 ? statusType : null);
-  }
-});
